7b7afd9703ac1211b4c9dfdf1d6b7d2ca7830c5d
Guido Martinez <martinez.guido14@gmail.com>
2020-10-06 16:36:36 -0300

    Dar alta cursos borrados, load

--- Codigo/ProjectoPAV/Program.cs
+++ Codigo/ProjectoPAV/Program.cs
@@ -18,7 +18,7 @@ namespace ProjectoPAV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu("asd"));
+            Application.Run(new Load());
         }
     }
 }
